fd1a6727bc55325724d0c7842bba9c8ae5145a20
Jenny Tzeng <j4427515@gmail.com>
2018-12-29 23:42:59 +0800

    first sql

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'watsons.apps.WatsonsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- watsons/models.py
+++ watsons/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    customer_name = models.CharField(max_length=30)
+    login_date = models.DateTimeField('date published')
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=30)
+
+
+class Order(models.Model):
+    check_date = models.DateTimeField('date published')
