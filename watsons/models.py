d861b2214fbd4a3478a78af2867e06fef5591072
Jenny Tzeng <j4427515@gmail.com>
2019-01-02 00:04:21 +0800

    database

--- watsons/models.py
+++ watsons/models.py
@@ -3,12 +3,28 @@ from django.db import models
 # Create your models here.
 class Customer(models.Model):
     customer_name = models.CharField(max_length=30)
-    login_date = models.DateTimeField('date published')
+    gender = models.IntegerField
+    birthday = models.DateTimeField
 
 
 class Product(models.Model):
     product_name = models.CharField(max_length=30)
+    category = models.IntegerField
+    price = models.IntegerField
+    brand = models.IntegerField  # could be a foreignkey ?
+    quantity = models.IntegerField
 
 
-class Order(models.Model):
-    check_date = models.DateTimeField('date published')
+class Transaction(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    time = models.DateTimeField('date published')
+    amount = models.IntegerField
+
+class Location(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+class Bonus(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    rate = models.FloatField
+    period = models.DateTimeField
