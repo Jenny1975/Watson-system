a0aa187be8ec5e1ee8f112cd17db6160f6a5f63a
Jenny Tzeng <j4427515@gmail.com>
2019-01-03 01:27:10 +0800

    database_all

--- watsons/models.py
+++ watsons/models.py
@@ -1,30 +1,60 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
 class Customer(models.Model):
+    MALE = 'M'
+    FEMALE = 'F'
+    OTHER = 'O'
+    gender_choice = (
+        (MALE, 'Male'),
+        (FEMALE, 'Female'),
+        (OTHER, 'Other'),
+    )
     customer_name = models.CharField(max_length=30)
-    gender = models.IntegerField
-    birthday = models.DateTimeField
+    gender = models.CharField(max_length=1, choices = gender_choice, default = MALE)
+    birthday = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.customer_name
+
+
 
 
 class Product(models.Model):
+    COSMETIC = 'CO'
+    SNACK = 'SA'
+    CARE_PRODUCT = 'CR'
+    category_choice = (
+        (COSMETIC, 'Cosmetic'),
+        (SNACK, 'Snacks'),
+        (CARE_PRODUCT, 'Care Product'),
+    )
     product_name = models.CharField(max_length=30)
-    category = models.IntegerField
-    price = models.IntegerField
+    category = models.CharField(max_length = 2, choices = category_choice, default = COSMETIC )
+    price = models.PositiveIntegerField()
     brand = models.IntegerField  # could be a foreignkey ?
-    quantity = models.IntegerField
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return self.product_name
 
 
 class Transaction(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    time = models.DateTimeField('date published')
-    amount = models.IntegerField
+    time = models.DateTimeField(blank=True, null=True)
+    amount = models.IntegerField()
+    
+
 
 class Location(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
 
 class Bonus(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    rate = models.FloatField
-    period = models.DateTimeField
+    rate = models.DecimalField(max_digits=5, decimal_places=2, default = 0)
+    period = models.DateTimeField(blank=True, null=True)
+
+
--- watsons/views.py
+++ watsons/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .model import Transaction
+from .models import Transaction
 
 # Create your views here.
 def index(request):
