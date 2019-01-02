789662772afe7c549d5cb142981e8d69eee8b373
Jenny Tzeng <j4427515@gmail.com>
2019-01-02 16:53:18 +0800

    index

--- watsons/views.py
+++ watsons/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .model import Transaction
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world.")
+    latest_transaction_list = Transaction.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('index.html')
+    context = {
+        'latest_transaction_list': latest_transaction_list,
+    }
+    return HttpResponse(template.render(context, request))
