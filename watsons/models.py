340c0748b4f6102f8e343719fe81026641db1370
Doggy1998 <44040901+Doggy1998@users.noreply.github.com>
2019-01-07 12:55:52 +0800

    銷量表

--- watsons/models.py
+++ watsons/models.py
@@ -1,14 +1,73 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Customer(models.Model):
+    MALE = 'M'
+    FEMALE = 'F'
+    OTHER = 'O'
+    gender_choice = (
+        (MALE, 'Male'),
+        (FEMALE, 'Female'),
+        (OTHER, 'Other'),
+    )
     customer_name = models.CharField(max_length=30)
-    login_date = models.DateTimeField('date published')
+    gender = models.CharField(max_length=1, choices = gender_choice, default = MALE)
+    birthday = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.customer_name
+
+
 
 
 class Product(models.Model):
+    COSMETIC = 'CO'
+    SNACK = 'SA'
+    CARE_PRODUCT = 'CR'
+    category_choice = (
+        (COSMETIC, 'Cosmetic'),
+        (SNACK, 'Snacks'),
+        (CARE_PRODUCT, 'Care Product'),
+    )
     product_name = models.CharField(max_length=30)
+    category = models.CharField(max_length = 2, choices = category_choice, default = COSMETIC )
+    price = models.PositiveIntegerField()
+    brand = models.IntegerField  # could be a foreignkey ?
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return self.product_name
+
+class Transaction(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    time = models.DateTimeField(blank=True, null=True)
+    amount = models.IntegerField()
+    def total(self):
+        amount = self.amount
+        product = self.product
+        total_amount = amount*product.price
+        return total_amount
 
 
-class Order(models.Model):
-    check_date = models.DateTimeField('date published')
+class Location(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    location_name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.location_name
+
+class Bonus(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    rate = models.DecimalField(max_digits=5, decimal_places=2, default = 0)
+    period = models.DateTimeField(blank=True, null=True)
+
+
+class Staff(models.Model):
+    phone = models.CharField(max_length=50, null=True)
+    isManager = models.BooleanField(default=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return User.objects.get(id=self.user_id).username
--- watsons/urls.py
+++ watsons/urls.py
@@ -3,6 +3,12 @@ from django.urls import path
 
 from . import views
 
+app_name = 'watsons'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('create', views.create, name='create'),
+    path('show_transaction', views.showTransaction, name='showTransaction'),
+
+
+
 ]
